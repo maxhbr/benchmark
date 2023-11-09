ee6c9a2593764543867310e1aad3acbf7a83357e
Maximilian Huber <gh@maxhbr.de>
2023-11-10 00:19:42 +0100

    add Beelink result
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- nix.run.sh
+++ nix.run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash -p acpi glmark2 fio linuxKernel.packages.linux_5_19.perf openjdk17 openscad ghc cabal-install gcc
+#! nix-shell -i bash -p acpi glmark2 fio linuxKernel.packages.linux_latest.perf openjdk17 openscad ghc cabal-install gcc
 exec ./run.sh "$@"
